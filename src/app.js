b4c866d9105f5eb58b3e48940b689b81b348aa5d
“Pablo <pmilitelloaveni@gmail.com>
2023-03-21 16:54:29 -0300

    agrego funcionalidad a realTimeProduct

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js';
 import cartsRouter from './routes/carts.router.js';
-import viewsRouter from './routes/views.router.js';
 import { __dirname } from './utils.js';
 import handlebars from 'express-handlebars';
-//import { Server } from 'socket.io';
+import viewsRouter from './routes/views.router.js';
+import { Server } from 'socket.io';
 
-const port = 8080;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 //Configuración Handlebars
 app.engine('handlebars', handlebars.engine());
@@ -24,28 +22,17 @@ app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use('/views', viewsRouter);
 
+const PORT = 8080;
+
 //Configuro el SocketServer
-const httpServer = app.listen(port, () => console.log(`Listen in port ${port}`));
+const httpServer = app.listen(PORT, () => console.log(`Listen in port ${PORT}`));
 
-//const messages = [];
+const socketServer = new Server(httpServer);
 
-//const socketServer = new Server(httpServer);
+socketServer.on('connection', (socket) => {
+  console.log(`Client conected id: ${socket.id}`);
 
-// socketServer.on('connection', (socket) => {
-//     console.log(`Client conected id: ${socket.id}`);
-
-//     socket.on('disconnect', () => {
-//       console.log(`Client disconected id: ${socket.id}`);
-//     });
-
-//     socket.emit('welcome', `Welcome to Websocket- Client id: ${socket.id}`);
-
-//     socket.on('answerWelcome', (text) => {
-//       console.log(text);
-//     });
-
-//     socket.on('message', (text) => {
-//       messages.push({ ClientID: socket.id, text });
-//       socketServer.emit('allMessages', messages);
-//     });
-//   });
+  socket.on('disconnect', () => {
+    console.log(`Client disconected id: ${socket.id}`);
+  });
+});
